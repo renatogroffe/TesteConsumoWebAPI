555ef485c5b75c0510d81c7b4def508cd367a33c
Renato Groffe <renato.groffe@yahoo.com.br>
2015-08-16 17:11:02 -0300

    Primeira publicação.

---
+++ TesteConsumoWebAPI/Controllers/ProdutosController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using TesteConsumoWebAPI.Models;
+
+namespace TesteConsumoWebAPI.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        public ActionResult ExemploConsumoDotNet()
+        {
+            List<Produto> produtos;
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = client.GetAsync(
+                    ConfigurationManager.AppSettings["UrlCatalogoProdutos"]).Result;
+                response.EnsureSuccessStatusCode();
+
+                produtos = response.Content.ReadAsAsync<List<Produto>>().Result;
+            }
+
+            return View(produtos);
+        }
+
+        public ActionResult ExemploConsumojQuery()
+        {
+            ViewBag.UrlCatalogoProdutos =
+                ConfigurationManager.AppSettings["UrlCatalogoProdutos"];
+            return View();
+        }
+    }
+}
---
+++ TesteConsumoWebAPI/Models/Produto.cs
@@ -0,0 +1,9 @@
+﻿namespace TesteConsumoWebAPI.Models
+{
+    public class Produto
+    {
+        public string CodProduto { get; set; }
+        public string NomeProduto { get; set; }
+        public double Preco { get; set; }
+    }
+}
